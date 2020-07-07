07a2c2173b25fb765ddad3d7979e25df623a3ef9
Cat <ccallcottstevens@gmail.com>
2020-07-07 22:12:41 +1000

    started building routes

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Login = () => {
+    return (
+        <div>
+            <h1>As you can see, I am a login form and should be the home page</h1>
+        </div>
+        
+    )
+}
+
+export default Login
