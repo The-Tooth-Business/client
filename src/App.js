f2643f603b90ecb49cc7fa864e7a3dd3f305005a
Cat <ccallcottstevens@gmail.com>
2020-07-08 21:20:34 +1000

    added a rudimentary nav with link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react'
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom' 
+import {BrowserRouter, Route, Switch } from 'react-router-dom' 
+import Nav from './components/Nav'
 // import NewBooking from './components/NewBooking'
 import Bookings from './components/Bookings'
 import Booking from './components/Booking'
@@ -8,6 +9,7 @@ import Logout from './components/Logout'
 import NotFound from './components/NotFound'
 import parentData from './data/parent_data'
 
+
 const App = () => {
   const [bookings, setBookings] = useState([])
   useEffect(()=> {
@@ -15,7 +17,7 @@ const App = () => {
   }, [])
 
 function getBookingFromId (id) {
-  return bookings.find((post) => post._id == id)
+  return bookings.find((post) => post._id === parseInt(id))
 }
 
   return (
@@ -24,11 +26,7 @@ function getBookingFromId (id) {
 
 
     <BrowserRouter>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/logout">Logout</Link></li>
-        <li><Link to="/bookings">Bookings</Link></li>
-      </ul>
+    <Nav />
         <Switch>
         <Route exact path='/' render={Login} />
         <Route exact path='/logout' render={Logout} />
