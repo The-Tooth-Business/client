85e1656547422db9d54df38a9bc26948b1acf112
Cat <ccallcottstevens@gmail.com>
2020-07-08 22:17:10 +1000

    create functionality added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React,{useState, useEffect} from 'react'
 import {BrowserRouter, Route, Switch } from 'react-router-dom' 
+import parentData from './data/parent_data'
 import Nav from './components/Nav'
 // import NewBooking from './components/NewBooking'
 import Bookings from './components/Bookings'
 import Booking from './components/Booking'
+import NewBooking from './components/NewBooking'
 import Login from './components/Login'
 import Logout from './components/Logout'
 import NotFound from './components/NotFound'
-import parentData from './data/parent_data'
+
 
 
 const App = () => {
@@ -26,17 +28,18 @@ function getBookingFromId (id) {
 
 
     <BrowserRouter>
-    <Nav />
+      <Nav />
         <Switch>
-        <Route exact path='/' render={Login} />
-        <Route exact path='/logout' render={Logout} />
-        <Route exact path='/bookings' render={(props) => <Bookings {...props} parentData={bookings} />}/>
-        <Route exact path='/posts/:id' render={(props) => 
+          <Route exact path='/' render={Login} />
+          <Route exact path='/logout' render={Logout} />
+          <Route exact path='/bookings' render={(props) => <Bookings {...props} parentData={bookings} />}/>
+          <Route exact path='/posts/:id' render={(props) => 
           <Booking {...props} post={getBookingFromId(props.match.params.id)} />}/>
-        <Route component={NotFound} />
+          <Route exact path='/booking/new' render={()=> <NewBooking /> } />
+          <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
-      </div>
+  </div>
   )
 }
 
