b568454bcd0adfba95f5675faf8f8a887302af8b
Cat <ccallcottstevens@gmail.com>
2020-07-14 09:58:12 +1000

    added workng cypress sign-in test

--- cypress/integration/users/loginLogout.spec.js
+++ cypress/integration/users/loginLogout.spec.js
@@ -5,15 +5,15 @@ describe('Test Register', () => {
         cy.get('[data-cy=register]').click()
         cy.url().should('include', '/auth/register')
     })
-    // it('should render SignIn component', () => {
-    //     cy.viewport(1024, 768)
-    //     cy.get('[data-cy=login]').click()
-    //     cy.get('[data-cy=loginForm]').should('be.visible')
-    // })
 })
 describe('Test Login', () => {
     it('should route to /auth/login',() => {
         cy.get('[data-cy=login]').click()
         cy.url().should('include', '/auth/login')
     })
+    it('Test should render SignIn component', () => {
+            cy.viewport(1024, 768)
+            cy.get('[data-cy=login]').click()
+            cy.get('[data-cy=loginForm]').should('be.visible')
+        })
 })
