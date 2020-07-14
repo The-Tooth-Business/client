55ada2bf4c9ff49a170f14d24083554cdf0f57a5
lou-flan <louise_flanagan@outlook.com>
2020-07-14 11:02:59 +1000

    Imported material icons. Imported responsive drawer into dashboard but need to refactor

---
+++ src/components/SideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const styles = {
+	width: 255,
+	height: '90vh',
+	background: 'thistle',
+};
+
+export default function SideNav() {
+	return <div style={styles}>menu goes here</div>;
+}
