784d68f593aa032adf6bd895dffa6ac346ca9be1
Cat <ccallcottstevens@gmail.com>
2020-07-15 16:54:52 +1000

    styled login form working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,67 @@
 import React, { useState } from 'react';
 import { loginUser } from '../services/authServices';
 import { useGlobalState } from '../config/globalState';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+	return (
+	  <Typography variant="body2" color="textSecondary" align="center">
+		{'Copyright © '}
+		<Link color="inherit" href="https://material-ui.com/">
+		  Your Website
+		</Link>{' '}
+		{new Date().getFullYear()}
+		{'.'}
+	  </Typography>
+	);
+  }
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  height: '100vh',
+	},
+	image: {
+	  backgroundImage: 'url(https://source.unsplash.com/random)',
+	  backgroundRepeat: 'no-repeat',
+	  backgroundColor:
+		theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+	  backgroundSize: 'cover',
+	  backgroundPosition: 'center',
+	},
+	paper: {
+	  margin: theme.spacing(8, 4),
+	  display: 'flex',
+	  flexDirection: 'column',
+	  alignItems: 'center',
+	},
+	avatar: {
+	  margin: theme.spacing(1),
+	  backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+	  width: '100%', // Fix IE 11 issue.
+	  marginTop: theme.spacing(1),
+	},
+	submit: {
+	  margin: theme.spacing(3, 0, 2),
+	},
+  }));
+
 
 const Login = ({ history }) => {
+	const classes = useStyles();
 	const { dispatch } = useGlobalState();
 	const initialFormState = {
 		username: '',
@@ -46,34 +105,82 @@ const Login = ({ history }) => {
 					);
 			});
 	}
+
 	return (
-		<form data-cy="login-form" onSubmit={handleSubmit}>
-			{errorMessage && <p>{errorMessage}</p>}
-			<div>
-				<label>Username</label>
-				<input
-					data-cy="username"
-					required
-					type="text"
-					name="username"
-					placeholder="Enter a username"
-					onChange={handleChange}
-				></input>
+		<Grid data-cy="login-form" onSubmit={handleSubmit}>
+			{errorMessage && <p>{errorMessage}</p>}container component="main" className={classes.root}>
+		  <CssBaseline />
+		  <Grid item xs={false} sm={4} md={7} className={classes.image} />
+		  <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+			<div className={classes.paper}>
+			  <Avatar className={classes.avatar}>
+				<LockOutlinedIcon />
+			  </Avatar>
+			  <Typography component="h1" variant="h5">
+				Login
+			  </Typography>
+			  <form className={classes.form} noValidate>
+				<TextField
+				data-cy="username"
+				  variant="outlined"
+				  margin="normal"
+				  required
+				  fullWidth
+				  id="username"
+				  label="Username"
+				  name="username"
+				  autoComplete="username"
+				  onChange={handleChange}
+				  autoFocus
+				/>
+				<TextField
+				  variant="outlined"
+				  margin="normal"
+				  required
+				  fullWidth
+				  name="password"
+				  label="Password"
+				  type="password"
+				  id="password"
+				  autoComplete="current-password"
+				  onChange={handleChange}
+				/>
+				<FormControlLabel
+				  control={<Checkbox value="remember" color="primary" />}
+				  label="Remember me"
+				/>
+				<Button
+				  type="submit"
+				  fullWidth
+				  variant="contained"
+				  color="primary"
+				  className={classes.submit}
+				  data-cy="login-button" 
+				>
+				  Login
+				</Button>
+				<Grid container>
+				  <Grid item xs>
+					<Link href="#" variant="body2">
+					  Forgot password?
+					</Link>
+				  </Grid>
+				  <Grid item>
+					<Link href="#" variant="body2">
+					  {"Don't have an account? Sign Up"}
+					</Link>
+				  </Grid>
+				</Grid>
+				<Box mt={5}>
+				<Copyright />
+				</Box>
+			  </form>
 			</div>
-			<div>
-				<label>Password</label>
-				<input
-					data-cy="password"
-					required
-					type="password"
-					name="password"
-					placeholder="Enter a password"
-					onChange={handleChange}
-				></input>
-			</div>
-			<input data-cy="login-button" type="submit" value="Login"></input>
-		</form>
-	);
+		  </Grid>
+		</Grid>
+	  );
+
+
 };
 
 export default Login;
