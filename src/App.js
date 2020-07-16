821ea2f9f83a6c466d0b8060739e2bd45b95ffdb
lou-flan <louise_flanagan@outlook.com>
2020-07-16 18:25:45 +1000

    removed unused nav component from app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import UserDashboard from './components/UserDashboard';
 import Bookings from './components/Bookings';
 import Booking from './components/Booking';
-import Nav from './components/Nav';
 import NewBooking from './components/NewBooking';
 import EditBooking from './components/EditBooking';
 import Login from './components/Login';
