fb4bb052cd009dd395fc1f104162eb729384f7bd
lou-flan <louise_flanagan@outlook.com>
2020-07-24 16:38:32 +1000

    Added modal pop up for bookings pending review

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,6 +4,11 @@ import Badge from '@material-ui/core/Badge';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { Link } from 'react-router-dom';
 
 const StyledBadge = withStyles((theme) => ({
 	badge: {
@@ -17,8 +22,17 @@ const StyledBadge = withStyles((theme) => ({
 export default function Alert() {
 	const { store } = useGlobalState();
 	const { bookings } = store;
+	const [open, setOpen] = React.useState(false);
 
-	const [pendingReview, setPendingReview] = useState('');
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const [pendingReview, setPendingReview] = useState([]);
 
 	useEffect(() => {
 		const pendingReviews = bookings.filter(
@@ -28,10 +42,39 @@ export default function Alert() {
 		return () => {};
 	}, [bookings]);
 	return (
-		<IconButton aria-label="cart">
-			<StyledBadge badgeContent={pendingReview.length || null} color="primary">
-				<NotificationsIcon />
-			</StyledBadge>
-		</IconButton>
+		<div>
+			<IconButton onClick={handleClickOpen}>
+				<StyledBadge
+					badgeContent={pendingReview.length || null}
+					color="primary"
+				>
+					<NotificationsIcon />
+				</StyledBadge>
+			</IconButton>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+			>
+				<DialogTitle id="alert-dialog-title">
+					{pendingReview.length > 0 && (
+						<p>
+							You have {pendingReview.length} bookings waiting to be reviewed
+						</p>
+					)}
+					{pendingReview.length === 0 && <p>You're all caught up!</p>}
+				</DialogTitle>
+				<DialogContent>
+					<DialogContentText id="alert-dialog-description">
+						{pendingReview.map((booking) => (
+							<Link to={`/bookings/${booking._id}`} onClick={handleClose}>
+								<p>{booking.child_name}</p>
+							</Link>
+						))}
+					</DialogContentText>
+				</DialogContent>
+			</Dialog>
+		</div>
 	);
 }
