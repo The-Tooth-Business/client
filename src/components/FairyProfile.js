36999196984fc79b60897a228b50fd6fae2e70ef
lou-flan <louise_flanagan@outlook.com>
2020-07-25 16:41:27 +1000

    Added some basic styling to fairy profile with dummy avatar.

--- src/components/FairyProfile.js
+++ src/components/FairyProfile.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { getFairyByContinent } from '../services/fairyServices';
+import Card from './Card';
+import { makeStyles } from '@material-ui/core/styles';
+import avatar from '../images/avatar.jpg';
+
+const useStyles = makeStyles((theme) => ({
+	toolbar: theme.mixins.toolbar,
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+	},
+	avatar: {
+		width: '300px',
+		height: '300px',
+		borderRadius: '50%',
+		backgroundImage: `url(${avatar})`,
+		backgroundRepeat: 'no-repeat',
+		opacity: 0.9,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+	},
+}));
 
 const FairyProfile = ({ history, continent }) => {
+	const classes = useStyles();
 	const initialState = {
 		continent: 'continent',
 		fairy_name: 'name',
 		description: 'description',
-	};
-
-	const divStyle = {
-		position: 'absolute',
-		right: '20%',
-		bottom: '40%',
 	};
 
 	const [fairyData, setFairyData] = useState(initialState);
@@ -25,12 +41,23 @@ const FairyProfile = ({ history, continent }) => {
 				console.log('An error occurred setting fairy data: ', error);
 			});
 	}, [continent]);
-	console.log(fairyData);
+
 	return (
-		<div style={divStyle}>
-			<h1>{fairyData.fairy_name}</h1>
-			<h2>The {fairyData.continent} Fairy</h2>
-			<p>{fairyData.description}</p>
+		<div>
+			<div className={classes.toolbar} />
+			<main className={classes.content}>
+				<div className={classes.avatar}> </div>
+				<Card
+					number={fairyData.fairy_name}
+					text={`The ${fairyData.continent} Fairy`}
+					background={
+						'linear-gradient(45deg, rgba(41,223,189,1) 62%, rgba(101,255,213,1) 96%)'
+					}
+				/>
+				{/* <h1>{fairyData.fairy_name}</h1>
+			<h2>The {fairyData.continent} Fairy</h2> */}
+				<p>{fairyData.description}</p>
+			</main>
 		</div>
 	);
 };
