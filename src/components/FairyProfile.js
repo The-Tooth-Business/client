2d1251481fc9344e563c9d479d59ed3441cae1c5
lou-flan <louise_flanagan@outlook.com>
2020-07-25 16:51:16 +1000

    Added GET req. in fair profile to fetch bookings from server for that fairy

--- src/components/FairyProfile.js
+++ src/components/FairyProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { getFairyByContinent } from '../services/fairyServices';
+import { getBookingsByContinent } from '../services/bookingsServices';
 import Card from './Card';
 import { makeStyles } from '@material-ui/core/styles';
 import avatar from '../images/avatar.jpg';
+import Bookings from './Bookings';
 
 const useStyles = makeStyles((theme) => ({
 	toolbar: theme.mixins.toolbar,
@@ -13,7 +15,6 @@ const useStyles = makeStyles((theme) => ({
 	avatar: {
 		width: '300px',
 		height: '300px',
-		borderRadius: '50%',
 		backgroundImage: `url(${avatar})`,
 		backgroundRepeat: 'no-repeat',
 		opacity: 0.9,
@@ -22,17 +23,20 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
-const FairyProfile = ({ history, continent }) => {
+const FairyProfile = ({ continent }) => {
 	const classes = useStyles();
 	const initialState = {
 		continent: 'continent',
 		fairy_name: 'name',
 		description: 'description',
 	};
-
+	const [fairyBookings, setFairyBookings] = useState([]);
 	const [fairyData, setFairyData] = useState(initialState);
 
 	useEffect(() => {
+		getBookingsByContinent(continent).then((bookings) => {
+			setFairyBookings(bookings);
+		});
 		getFairyByContinent(continent)
 			.then((fairy) => {
 				setFairyData(fairy);
@@ -47,6 +51,7 @@ const FairyProfile = ({ history, continent }) => {
 			<div className={classes.toolbar} />
 			<main className={classes.content}>
 				<div className={classes.avatar}> </div>
+				<p>{fairyData.description}</p>
 				<Card
 					number={fairyData.fairy_name}
 					text={`The ${fairyData.continent} Fairy`}
@@ -54,9 +59,8 @@ const FairyProfile = ({ history, continent }) => {
 						'linear-gradient(45deg, rgba(41,223,189,1) 62%, rgba(101,255,213,1) 96%)'
 					}
 				/>
-				{/* <h1>{fairyData.fairy_name}</h1>
-			<h2>The {fairyData.continent} Fairy</h2> */}
-				<p>{fairyData.description}</p>
+
+				<Bookings bookings={fairyBookings} />
 			</main>
 		</div>
 	);
