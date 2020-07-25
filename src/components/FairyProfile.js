3adb633924db640726042bc09f37605c41d54494
lou-flan <louise_flanagan@outlook.com>
2020-07-25 15:07:07 +1000

    Added state to fairyProfile and useEffect to make HTTP request and set state

--- src/components/FairyProfile.js
+++ src/components/FairyProfile.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { useGlobalState } from '../config/globalState';
+// import { useGlobalState } from '../config/globalState';
+import { getFairyByContinent } from '../services/fairyServices';
 
 const FairyProfile = ({ history, continent }) => {
-	const { store, dispatch } = useGlobalState();
-	const [errorMessage, setErrorMessage] = useState(null);
+	// const { store, dispatch } = useGlobalState();
+	// const [errorMessage, setErrorMessage] = useState(null);
+	const initialState = {
+		continent: '',
+		fairy_name: '',
+		description: '',
+	};
 
-	// useEffect(() => {
-	//     getFairyData(continent)
-	// })
+	const [fairyData, setFairyData] = useState(initialState);
 
-	return <div></div>;
+	useEffect(() => {
+		getFairyByContinent(continent).then((fairy) => {
+			setFairyData(fairy);
+		});
+	});
+
+	return <div>{fairyData}</div>;
 };
 
 export default withRouter(FairyProfile);
