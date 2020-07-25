6e0601dd24e59967ae5229903e1e2e442a98d439
Cat <ccallcottstevens@gmail.com>
2020-07-26 07:35:38 +1000

    changed colour on circular integration

--- src/components/CircularIntegration.js
+++ src/components/CircularIntegration.js
@@ -3,7 +3,7 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
-// import { green } from '@material-ui/core/colors';
+import { green } from '@material-ui/core/colors';
 import Button from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
 import CheckIcon from '@material-ui/icons/Check';
@@ -20,20 +20,20 @@ const useStyles = makeStyles((theme) => ({
     position: 'relative',
   },
   buttonSuccess: {
-    backgroundColor: '#009688',
+    backgroundColor: green[500],
     '&:hover': {
-      backgroundColor: '#009688',
+      backgroundColor: green[700],
     },
   },
   fabProgress: {
-    color: '#009688',
+    color: green[500],
     position: 'absolute',
     top: -6,
     left: -6,
     zIndex: 1,
   },
   buttonProgress: {
-    color: "#009688",
+    color: green[500],
     position: 'absolute',
     top: '50%',
     left: '50%',
@@ -78,7 +78,7 @@ export default function CircularIntegration({handleSubmit, booking}) {
       <div className={classes.wrapper}>
         <Fab
           aria-label="save"
-          color="secondary"
+          color="primary"
           className={buttonClassname}
           onClick={handleButtonClick} 
         >
@@ -89,7 +89,7 @@ export default function CircularIntegration({handleSubmit, booking}) {
       <div className={classes.wrapper}>
         <Button
           variant="contained"
-          color="secondary"
+          color="primary"
           className={buttonClassname}
           disabled={loading}
           onClick={handleButtonClick}
