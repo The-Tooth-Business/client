88abba89c2371db11d40ae45e8fe3760eacefbee
Cat <ccallcottstevens@gmail.com>
2020-07-27 15:50:22 +1000

    images rendering for each continent profile

--- src/components/FairyProfile.js
+++ src/components/FairyProfile.js
@@ -3,69 +3,45 @@ import { getFairyByContinent } from '../services/fairyServices';
 import { getBookingsByContinent } from '../services/bookingsServices';
 import Card from './Card';
 import { makeStyles } from '@material-ui/core/styles';
-// import avatar from '../images/avatar.jpg';
 import Asia from '../images/Asia.jpeg';
-import Africa from '../images/Africa.jpeg';
-import Antarctica from '../images/Antarctica.jpeg';
 import NorthAmerica from '../images/NorthAmerica.jpeg';
 import SouthAmerica from '../images/SouthAmerica.jpeg';
 import Europe from '../images/Europe.jpeg';
 import Oceania from '../images/Oceania.jpeg';
+import Antarctica from '../images/Antarctica.jpeg';
+import Africa from '../images/Africa.jpeg';
 import Bookings from './Bookings';
 import { getAverageRating, totalTeeth } from '../utils/calculations';
 import { useGlobalState } from '../config/globalState';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles((theme) => ({
-	toolbar: theme.mixins.toolbar,
-	content: {
-		flexGrow: 1,
-		padding: theme.spacing(3),
-	},
-	avatar: {
-		width: '100%',
-		height: '300px',
-		
-		backgroundRepeat: 'no-repeat',
-		opacity: 0.9,
-		backgroundSize: 'cover',
-		backgroundPosition: 'center',
-	},
-	paper: {
-		padding: theme.spacing(2),
-		textAlign: 'left',
-		height: '100%',
-		width: '100%',
-	},
-}));
-
-
 const FairyProfile = ({ continent }) => {
 	const { store } = useGlobalState();
 	const { adminUser } = store;
-	const classes = useStyles();
+
 	const initialState = {
 		continent: 'continent',
 		fairy_name: 'name',
 		description: 'description',
 	};
-	const Images = [
-		Asia,
-		Africa,
-		Oceania,
-		NorthAmerica,
-		SouthAmerica,
-		Antarctica,
-		Europe
-	];
-	const [fairyBookings, setFairyBookings, fairyImages, setFairyImages] = useState([]);
+	const [fairyBookings, setFairyBookings] = useState([]);
 	const [fairyData, setFairyData] = useState(initialState);
 	const [fairyRating, setFairyRating] = useState(0);
-	
-	
+	const [image, setImage] = useState([]);
 
 	useEffect(() => {
+		const imgs = [
+			{ name: 'Asia', url: Asia },
+			{ name: 'Europe', url: Europe },
+			{ name: 'North America', url: NorthAmerica },
+			{ name: 'South America', url: SouthAmerica },
+			{ name: 'Africa', url: Africa },
+			{ name: 'Antarctica', url: Antarctica },
+			{ name: 'Oceania', url: Oceania },
+		];
+		const profile = imgs.find((img) => img.name === continent).url;
+		setImage(profile);
 		getBookingsByContinent(continent).then((bookings) => {
 			setFairyBookings(bookings);
 			setFairyRating(getAverageRating(bookings));
@@ -79,7 +55,30 @@ const FairyProfile = ({ continent }) => {
 			});
 	}, [continent]);
 
-	
+	const useStyles = makeStyles((theme) => ({
+		toolbar: theme.mixins.toolbar,
+		content: {
+			flexGrow: 1,
+			padding: theme.spacing(3),
+		},
+		avatar: {
+			width: '100%',
+			height: '300px',
+			backgroundImage: `url(${image})`,
+			backgroundRepeat: 'no-repeat',
+			opacity: 0.9,
+			backgroundSize: 'cover',
+			backgroundPosition: 'center',
+		},
+		paper: {
+			padding: theme.spacing(2),
+			textAlign: 'left',
+			height: '100%',
+			width: '100%',
+		},
+	}));
+	const classes = useStyles();
+
 	return (
 		<div>
 			<div className={classes.toolbar} />
@@ -87,27 +86,8 @@ const FairyProfile = ({ continent }) => {
 				<Grid container spacing={3}>
 					<Grid item xs={12} lg={6}>
 						<Paper className={classes.paper}>
-							<div className={classes.avatar}> 
-							{/* let fairyImage = null;
-							switch(this.state.continents) {
-			
-									case "Asia":
-									return <Image style={backgroundImage} source={require('../images/Asia.jpeg')} />;
-									case "Africa":
-									return <Image style={backgroundImage} source={require('../images/Africa.jpeg')} />;
-									case "North America":
-									return <Image style={backgroundImage} source={require('../images/NorthAmerica.jpeg')} />;
-									case "South America":
-									return <Image style={backgroundImage} source={require('../images/SouthAmerica.jpeg')} />;
-									case "Europe":
-									return <Image style={backgroundImage} source={require('../images/Europe.jpeg')} />;
-									case "Oceania":
-									return <Image style={backgroundImage} source={require('../images/Oceania.jpeg')} />;
-									case "Antarctica":
-									return <Image style={backgroundImage} source={require('../images/Antarctics.jpeg')} />;
-							} */}
-						
-						</div>
+							<div className={classes.avatar} />
+							{/* <img src={image} alt="profile" /> */}
 							<div>
 								<h1>{fairyData.fairy_name}</h1>
 								<h2>The {fairyData.continent} Fairy</h2>
