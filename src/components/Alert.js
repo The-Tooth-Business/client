d98d7777b7bea7c25f3f4cbabff37743e34aa899
lou-flan <louise_flanagan@outlook.com>
2020-07-28 16:10:38 +1000

    Refactored Alert.js to display the number of wishes to review for the admin user rather than bookings that need to be reviewed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,6 +3,7 @@ import { useGlobalState } from '../config/globalState';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { getWishes } from '../services/wishServices';
 import {
 	Badge,
 	IconButton,
@@ -24,8 +25,9 @@ const StyledBadge = withStyles((theme) => ({
 
 export default function Alert() {
 	const { store } = useGlobalState();
-	const { bookings } = store;
+	const { bookings, adminUser } = store;
 	const [open, setOpen] = React.useState(false);
+	const [wishes, setWishes] = React.useState(false);
 
 	const handleClickOpen = () => {
 		setOpen(true);
@@ -41,13 +43,19 @@ export default function Alert() {
 		const pendingReviews = bookings.filter(
 			(booking) => !booking.review_status && !booking.open_status
 		);
+		getWishes().then((wishes) => {
+			setWishes(wishes.length);
+		});
 		setPendingReview(pendingReviews);
 		return () => {};
 	}, [bookings]);
 	return (
 		<div>
 			<IconButton onClick={handleClickOpen}>
-				<StyledBadge badgeContent={pendingReview.length || null} color="primary">
+				<StyledBadge
+					badgeContent={adminUser ? wishes || null : pendingReview.length || null}
+					color="secondary"
+				>
 					<NotificationsIcon />
 				</StyledBadge>
 			</IconButton>
@@ -57,28 +65,38 @@ export default function Alert() {
 				aria-labelledby="alert-dialog-title"
 				aria-describedby="alert-dialog-description"
 			>
-				<DialogTitle id="alert-dialog-title">
-					{pendingReview.length > 0 && (
-						<p>You have {pendingReview.length} bookings waiting to be reviewed</p>
-					)}
-					{pendingReview.length === 0 && <p>You're all caught up!</p>}
-				</DialogTitle>
-				<DialogContent>
-					<DialogContentText id="alert-dialog-description">
-						{pendingReview.map((booking, index) => (
-							<Button key={`${index}-${booking._id}`}>
-								{' '}
-								<Link
-									key={`${index}-${booking._id}`}
-									to={`/bookings/${booking._id}`}
-									onClick={handleClose}
-								>
-									{booking.child_name}
-								</Link>
-							</Button>
-						))}
-					</DialogContentText>
-				</DialogContent>
+				{adminUser && (
+					<DialogTitle id="alert-dialog-title">
+						{wishes > 0 && <p>You have {wishes} wishes waiting to be reviewed</p>}
+						{wishes === 0 && <p>You're all caught up!</p>}
+					</DialogTitle>
+				)}
+				{!adminUser && (
+					<DialogTitle id="alert-dialog-title">
+						{pendingReview.length > 0 && (
+							<p>You have {pendingReview.length} bookings waiting to be reviewed</p>
+						)}
+						{pendingReview.length === 0 && <p>You're all caught up!</p>}
+					</DialogTitle>
+				)}
+				{!adminUser && (
+					<DialogContent>
+						<DialogContentText id="alert-dialog-description">
+							{pendingReview.map((booking, index) => (
+								<Button key={`${index}-${booking._id}`}>
+									{' '}
+									<Link
+										key={`${index}-${booking._id}`}
+										to={`/bookings/${booking._id}`}
+										onClick={handleClose}
+									>
+										{booking.child_name}
+									</Link>
+								</Button>
+							))}
+						</DialogContentText>
+					</DialogContent>
+				)}
 			</Dialog>
 		</div>
 	);
